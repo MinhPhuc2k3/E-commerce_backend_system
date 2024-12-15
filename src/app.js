d55d61bd942b7ec0d933486dacbf32ed7e00e9c3
MinhPhuc2k3 <leminhphuc349@gmail.com>
2024-12-15 22:43:14 +0700

    Khoi tao du an

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
+const app = express()
+ 
+// init  middleware
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+// app.use(morgan('combine'))
+// app.use(morgan('common'))
+// app.use(morgan('short'))
+// app.use(morgan('tiny'))
+
+//init db
+
+//init routes
+app.get('/',(req, res, next) => {
+    const strCompress = "Javascripts"
+    return res.status(200).json({
+        'message': 'Welcome to TipJS 2',
+        metadata: strCompress.repeat(100000)
+    })
+})
+//handling error
+module.exports = app
